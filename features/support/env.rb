430175b10c29dec6d5a7d0bb0c63b9de1464e802
Sam Goldman <samwgoldman@gmail.com>
2012-05-19 16:18:15 -0400

    Try to make JRuby boot faster to avoid timeouts

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,3 +2,11 @@ require "bundler"
 Bundler.setup
 
 require "aruba/cucumber"
+
+# Try to make JRuby boot faster to avoid timeouts
+Aruba.configure do |config|
+  config.before_cmd do |cmd|
+    set_env("JRUBY_OPTS", "-X-C #{ENV["JRUBY_OPTS"]}")
+    set_env("JAVA_OPTS", "-d32 #{ENV["JAVA_OPTS"]}")
+  end
+end if RUBY_PLATFORM == "java"
